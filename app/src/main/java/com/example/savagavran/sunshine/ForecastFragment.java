1ed9117febeca68585d728dfc763ab37a47aba3e
sava.gavran <gavransava@gmail.com>
2016-11-15 14:54:58 +0100

    Sunshine - Lesson 5 pt3

--- app/src/main/java/com/example/savagavran/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/savagavran/sunshine/ForecastFragment.java
@@ -1,6 +1,5 @@
 package com.example.savagavran.sunshine;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -57,6 +56,19 @@ public class ForecastFragment extends Fragment
     static final int COL_COORD_LONG = 8;
 
     private ForecastAdapter mForecastAdapter;
+
+    /**
+     +     * A callback interface that all activities containing this fragment must
+     +     * implement. This mechanism allows activities to be notified of item
+     +     * selections.
+     +     */
+    public interface Callback {
+        /**
+         * DetailFragmentCallback for when an item has been selected.
+         */
+        public void onItemSelected(Uri dateUri);
+    }
+
 
     public ForecastFragment() {
     }
@@ -117,11 +129,9 @@ public class ForecastFragment extends Fragment
                 Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
                 if (cursor != null) {
                     String locationSetting = Utility.getPreferredLocation(getActivity());
-                    Intent intent = new Intent(getActivity(), DetailActivity.class)
-                            .setData(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(
+                    ((Callback) getActivity()).onItemSelected(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(
                                     locationSetting, cursor.getLong(COL_WEATHER_DATE)
                             ));
-                    startActivity(intent);
                 }
             }
         });
